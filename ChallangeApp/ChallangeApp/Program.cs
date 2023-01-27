bc824c172e4cff4ea0b009e28447c38c3bb3d260
Kordowski <rafal@kordowski.pl>
2023-01-27 19:14:49 +0100

    Praca domowa dzień 5

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,60 +1,52 @@
-﻿var age = 0;
-var isMale = true;
-var name = "";
-var check = "";
+﻿int number;
+string userNumberInString;
+int zero=0,jeden=0, dwa=0, trzy=0, cztery = 0, piec = 0, szesc = 0, siedem = 0, osiem = 0, dziewiec = 0;
 
+Console.WriteLine("Podaj liczbę do policzenia: ");
+userNumberInString = Console.ReadLine();
+DigitCounter(userNumberInString);
 
-Console.WriteLine("Podaj imie do sprawdzenia: ");
-name = Console.ReadLine();
-Console.WriteLine("Podaj wiek do sprawdzenia: ");
-age = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Jakiej jesteś płci? Mężczyzna czy Kobieta?");
-check = Console.ReadLine();
-
-if (check == "Mężczyzna")
+void DigitCounter (string NumberInString)
 {
-    isMale = true;
-}
-else if (check == "Kobieta")
-{
-    isMale = false;
-}
-else
-{
-    Console.Error.WriteLine("Podałeś złą płeć");
-    return;
-}
+    char[] letters = NumberInString.ToArray();
+    foreach (char letter in letters)
+    {
+        Console.WriteLine(letter);
+        if(letter == '0')
+        { zero++; }
+        else if (letter == '1')
+        { jeden++; }
+        else if (letter == '2')
+        { dwa++; }
+        else if (letter == '3')
+        { trzy++; }
+        else if (letter == '4')
+        { cztery++; }
+        else if (letter == '5')
+        { piec++; }
+        else if (letter == '6')
+        { szesc++; }
+        else if (letter == '7')
+        { siedem++; }
+        else if (letter == '8')
+        { osiem++; }
+        else
+        { dziewiec++; }
+    }
+    Console.WriteLine("Liczba po przeliczeniu ma następującą ilość cyfr:");
+    Console.WriteLine("0 ==> " + zero + "razy");
+    Console.WriteLine("1 ==> " + jeden + "razy");
+    Console.WriteLine("2 ==> " + dwa + "razy");
+    Console.WriteLine("3 ==> " + trzy + "razy");
+    Console.WriteLine("4 ==> " + cztery + "razy");
+    Console.WriteLine("5 ==> " + piec + "razy");
+    Console.WriteLine("6 ==> " + szesc + "razy");
+    Console.WriteLine("7 ==> " + siedem + "razy");
+    Console.WriteLine("8 ==> " + osiem + "razy");
+    Console.WriteLine("9 ==> " + dziewiec + "razy");
+    
 
-
-if (isMale)
-{
-    Console.WriteLine("Mężczyzna");
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni");
-    }
-    else
-    {
-        Console.WriteLine("Pełnoletni");
-    }
 }
-else
-{
-    if (age < 30)
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-    else if (name == "Ewa")
-    {
-        Console.WriteLine("Ewa, lat 33");
-    }
-    else
-    {
-        Console.WriteLine("Inna kobieta powyżej 30 lat");
-    }
-}
-
-
 
 
 
